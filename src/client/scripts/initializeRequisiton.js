a3a9f62bc3132f5f344a2491852a03b0a618c8af
Dezotti <brilhante.vini@gmail.com>
2022-12-10 15:46:06 -0300

    change index to interface

--- src/client/scripts/initializeRequisiton.js
+++ src/client/scripts/initializeRequisiton.js
@@ -18,28 +18,6 @@ window.onload = ('load', function () {
     updateSelect();
 })
 
-function updateSelect(){
-    const selectProviders = document.getElementById("selectProviders");
-    const selectGenres = document.getElementById("selectGenres");
-
-    let dataProvides = '';
-    let dataGenres = '';
-
-    for(let i=0; i<providers.total_results; i++)
-    {
-        dataProvides += `<option value="${providers.results[i].provider_id}">
-        ${providers.results[i].provider_name}</option>`;
-    }
-    selectProviders.innerHTML = dataProvides;
-
-    for(let i=0; i<genres.total_results; i++)
-    {
-        dataGenres += `<option value="${genres.results[i].genre_id}">
-        ${genres.results[i].genre_name}</option>`;
-    }
-    selectGenres.innerHTML = dataGenres;
-}
-
 async function getRequest(url) {
     let data = await fetch('http://localhost:4000' + url)
         .then((response) => response.json())
